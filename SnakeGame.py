11165378d80d478ce3b2bb706d3103ca08216140
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2021-04-28 22:54:05 +0200

    renamed files, can now hold the p button to print the map

---
+++ SnakeGame.py
@@ -0,0 +1,323 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 18:19:43 2021
+
+@author: Runar
+"""
+
+import numpy as np
+import random
+import keyboard
+import time
+
+from os import system
+        
+        
+
+directionDict = {"up":0,"right":1,"down":2,"left":3}
+vectorToDirectionDict = {(-1,0):0,(0,1):1,(1,0):2,(0,-1):3}
+
+class Fruit:
+    def __init__(self,mapSize):
+        self.__mapSize = mapSize
+        self.newPosition()
+    
+    def newPosition(self):
+        self.__position = Coordinate(random.randint(1,self.__mapSize-2),random.randint(1,self.__mapSize-2))
+        return self.__position
+    
+    def getPosition(self):
+        return self.__position
+    
+    
+
+class Coordinate:
+    def __init__(self,y,x):
+        self.__y = y
+        self.__x = x
+    
+    def getX(self):
+        return self.__x
+    
+    
+    def getY(self):
+        return self.__y
+    
+    
+    def __add__(self,movement):
+        if(type(movement) != list and type(movement) != tuple):
+            raise Exception("Addition on coordinate must be used on a tuple or list")
+        return Coordinate(self.__y + movement[0], self.__x + movement[1])
+    
+    def __str__(self):
+        return "Y: "+ str(self.__y) + " X: " + str(self.__x)
+    
+    def __eq__(self,coord):
+        return self.__y == coord.getY() and self.__x == coord.getX()
+    
+    
+    
+    
+        
+
+class Snake:
+    def __init__(self,startCoordinate,startDirection):
+        #print("Snake Constructor called")
+        if(type(startCoordinate)==Coordinate):
+            self.__headPosition = startCoordinate
+        elif(type(startCoordinate)==tuple or type(startCoordinate)==list):
+            self.__headPosition = Coordinate(startCoordinate[0],startCoordinate[1])
+        else:
+            sCType = str(type(startCoordinate))
+            raise Exception("startCoordinate was of type" + sCType)
+        self.__bodyPositions = []
+        
+        if(startDirection == directionDict["up"]):
+            self.__bodyPositions.append(self.__headPosition + [1,0])
+        elif(startDirection == directionDict["right"]):
+            self.__bodyPositions.append(self.__headPosition + [0,-1])
+        elif(startDirection == directionDict["down"]):
+            self.__bodyPositions.append(self.__headPosition + [-1,0])
+        elif(startDirection == directionDict["left"]):
+            self.__bodyPosition.append(self.__headPosition + [0,1])
+        #self.__bodyPositions = [Coordinate(self.__headPosition.getY() +1,self.__headPosition.getX())]
+        self.__direction = startDirection
+        
+    def move(self,movement):
+        if(type(movement) != list and type(movement) != tuple):
+            raise Exception("movement parameter must be a tuple or list")
+        lastBodyPosition = self.__headPosition
+        self.__headPosition = self.__headPosition + movement
+       
+        for i in range(len(self.__bodyPositions)):
+            self.__bodyPositions[i], lastBodyPosition = lastBodyPosition , self.__bodyPositions[i]
+            #print(lastBodyPosition)
+        self.__behindTailPosition = lastBodyPosition
+        self.__direction = vectorToDirectionDict[(self.__headPosition.getY()-self.__bodyPositions[0].getY(),
+                                                  self.__headPosition.getX()-self.__bodyPositions[0].getX())]
+        return self.__headPosition
+        
+    
+    def eatFruit(self):
+        self.__bodyPositions.append(self.__behindTailPosition)
+        
+     
+    def getDirection(self):
+        return self.__direction
+    
+    def fetchPositions(self):
+        yield self.__headPosition,"h"
+        for bodyPosition in self.__bodyPositions:
+            yield bodyPosition,"b"
+            
+    def getOldTailPosition(self):
+        return self.__behindTailPosition
+        
+          
+        
+        
+
+class SnakeMap:
+    def __init__(self,size,mapForUser):
+        #print("Snake Map Constructor called")
+        if(mapForUser):
+            self.__mapValues = {"empty":" ","wall":"w","head":"h","body":"b","fruit":"f"}
+            self.__map = np.full(shape=(size,size),fill_value=" ",dtype=str)
+        else:
+            self.__mapValues = {"empty":0,"wall":-4,"head":1,"body":-1,"fruit":4}
+            self.__map = np.full(shape=(size,size),fill_value=0,dtype=np.int32)
+        
+        self.__snake = Snake((size-3,size-3),directionDict["up"])
+        self.__fruit = Fruit(size)
+        self.__size = size
+        self.__updateSnake()
+        self.__updateFruit()
+        self.__setBorderWalls()
+        self.__gameover = False
+        
+    
+    def doTick(self,action):
+        snakeDirection = self.__snake.getDirection()
+        
+        if(action == 0):
+            newDirection = (snakeDirection + 5) % 4
+        elif(action == 2):
+            newDirection = (snakeDirection + 1) % 4
+        else:
+            newDirection = snakeDirection
+        
+        if(newDirection  == 0):
+            newPosition = self.__snake.move((-1,0))
+        elif(newDirection  == 1):
+            newPosition = self.__snake.move((0,1))
+        elif(newDirection  == 2):
+            newPosition = self.__snake.move((1,0))   
+        else:
+            newPosition = self.__snake.move((0,-1))     
+        
+        additionalScore = 0
+        #print(newPosition)
+        #print(self.__atPos(newPosition))
+        #print(self.__map)
+        if(self.__atPos(newPosition) == self.__mapValues["wall"] or self.__atPos(newPosition) == self.__mapValues["body"]):
+            self.__gameover = True
+            additionalScore = -1000
+            
+        else:
+            self.__changeMap(self.__snake.getOldTailPosition(),self.__mapValues["empty"])
+            
+        self.__updateSnake()
+        #print(newPosition,self.__fruit.getPosition())
+        if(newPosition == self.__fruit.getPosition()):
+            additionalScore = 1000
+            #print("Fruit eaten!")
+            self.__snake.eatFruit()
+            while True:
+                if(self.__atPos(self.__fruit.newPosition()) == self.__mapValues["empty"]):
+                    self.__updateFruit()
+                    break
+        elif(not self.__gameover):
+            additionalScore = -1
+        self.__updateFruit()
+        return additionalScore,self.__snake.getDirection()
+        
+            
+    def getMap(self):
+        return self.__map.copy()
+            
+    def isGameOver(self):
+        return self.__gameover
+    
+    def __updateSnake(self):
+        for position,part in self.__snake.fetchPositions():
+            if(part == "h"):
+                p = self.__mapValues["head"]
+            elif(part=="b"):
+                p = self.__mapValues["body"]
+            self.__changeMap(position,p)
+            
+    def __updateFruit(self):
+        self.__changeMap(self.__fruit.getPosition(), self.__mapValues["fruit"])
+    
+    def __changeMap(self,position, value):
+        self.__map[position.getY()][position.getX()] = value
+                
+    
+        
+    def __str__(self):
+        return str(self.__map)
+    
+
+    def __atPos(self,position):
+        return self.__map[position.getY()][position.getX()]
+    
+    def __setBorderWalls(self):
+        self.__map[0,:]=self.__mapValues["wall"]
+        self.__map[self.__size - 1,:] = self.__mapValues["wall"]
+        self.__map[1:self.__size-1,0] = self.__mapValues["wall"]
+        self.__map[1:self.__size-1,self.__size-1] = self.__mapValues["wall"]
+        
+        
+    
+    
+class SnakeGame:
+    def __init__(self,size,userPlayed):
+        #print("Snake Game Constructor called")
+        self.__userPlayed = userPlayed
+        self.__snakeMap = SnakeMap(size,userPlayed)
+        self.__directions={"up":(-1,0),"down":(1,0),"left":(0,-1),"right":(0,1)}
+        self.__direction = self.__directions["up"]
+        self.__lastTick = 0
+        self.__score = 0
+        self.__gameSize = size
+        self.__snakeDirection = 0
+        
+        #print(self.__lastTick)
+    
+    def reset(self):
+        self.__snakeMap = SnakeMap(self.__gameSize,self.__userPlayed)
+        self.__direction = self.__directions["up"]
+        self.__lastTick = 0
+        self.__score = 0
+    
+    def updateDirection(self):
+        if(keyboard.is_pressed('w')):
+            self.__direction=self.__directions["up"]
+        elif(keyboard.is_pressed('a')):
+            self.__direction=self.__directions["left"]
+        elif(keyboard.is_pressed('s')):
+            self.__direction=self.__directions["down"]
+        elif(keyboard.is_pressed('d')):
+            self.__direction=self.__directions["right"]
+        return keyboard.is_pressed("Esc")
+        
+    def checkForTick(self):
+        if(time.time() - self.__lastTick > 1.0):
+            self.__lastTick = time.time()
+            self.__score += self.__snakeMap.doTick(self.__direction)
+            system('cls')
+            print("Score:",self.__score)
+            print(self.__snakeMap)
+            return self.__snakeMap.isGameOver()
+        
+    
+    def step(self,action,doPrint):
+        '''
+        if(action == 0):
+            self.__direction = self.__directions["up"]
+        elif(action == 1):
+            self.__direction = self.__directions["right"]
+        elif(action == 2):
+            self.__direction = self.__directions["down"]
+        elif(action == 3):
+            self.__direction = self.__directions["left"]
+        else:
+            raise Exception("Was given invalid action")
+        '''
+        
+        
+       
+        
+        reward,self.__snakeDirection = self.__snakeMap.doTick(action)
+        directionObservation = np.zeros(self.__gameSize)
+        directionObservation[self.__snakeDirection] = 1
+        newObservation = np.append(self.__snakeMap.getMap(),[directionObservation],axis=0)
+        done = self.__snakeMap.isGameOver()
+        
+        self.__score += reward
+        if doPrint:
+            system('cls')
+            print("Score:",self.__score)
+            print(self.__snakeMap)
+        return reward,newObservation,done
+    
+    
+    def getObservation(self):
+        directionObservation = np.zeros(self.__gameSize)
+        directionObservation[self.__snakeDirection] = 1
+        return np.append(self.__snakeMap.getMap(),[directionObservation],axis=0)
+    
+    def getScore(self):
+        return self.__score
+    
+        
+            
+            
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+
---
+++ snakeMLMain.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 18:18:49 2021
+
+@author: Runar
+"""
+
+from SnakeGame import SnakeGame
+from DeepQAgent import Agent
+import time
+import keyboard
+from os import system
+import matplotlib.pyplot as plt
+
+
+
+
+'''    
+game = SnakeGame()
+gameExited = False
+gameGameover = False
+while (not gameExited and not gameGameover):
+    gameExited = game.updateDirection()
+    gameGameover = game.checkForTick()
+    
+if(gameGameover):
+    print("Game Over!")
+    input()
+'''
+
+amountOfGames = 3000
+snakeGameSize = 10
+game = SnakeGame(snakeGameSize,False)
+loadModel = False
+
+#memSize 5000 gamma .92 epsilonMin .07 no sucsess
+
+
+agent = Agent(gamma=0.96,epsilon=1,lr=1e-3,inputDims=(11,10),epsilonDec=1e-3,memSize=7000,
+              batchSize=64,epsilonMin=0.07,fc1Dims=256,fc2Dims=256,fc3Dims=128,replace=100,nActions=3)
+
+obs = game.getObservation()
+agent.prepNetworksForLoad(obs)
+if(loadModel):
+    agent.loadModel()
+
+#percentage = 0
+
+doPrint = False
+
+scores = []
+matchNumbers = []
+
+
+for i in range(amountOfGames):
+    #print("Game:",i)
+    done = False
+    observation = game.getObservation()
+    lastAction = 0
+    direction = 0
+    '''
+    if i > 1000 and i < 1100:
+        doPrint = True
+    elif i > 2000 and i < 2100:
+        doPrint = True
+    else:
+        doPrint = False
+    '''
+    while not done:
+        if(keyboard.is_pressed('p')):
+            doPrint = True
+        else:
+            doPrint = False
+        
+        action = agent.chooseAction(observation)
+        
+        reward, observation_, done = game.step(action,doPrint)
+        agent.storeTransition(observation, action, reward, observation_, done)
+        observation = observation_
+        agent.learn()
+        lastAction = action
+        if doPrint:
+            time.sleep(0.1)
+    score = game.getScore()
+    if(keyboard.is_pressed("Esc")):
+        break
+    scores.append(score)
+    matchNumbers.append(i)
+    print("Game %i ended with score %i" %(i,score))
+    '''
+    if((i//amountOfGames)* 100 != percentage):
+        percentage = i//amountOfGames * 100
+        system("cls")
+        print(str(i) + "%")
+        '''
+    game.reset()
+
+
+print("Done!")
+plt.plot(matchNumbers,scores)
+plt.show()
+
+inp = input("Save model (y/n)").lower()
+if(inp == "y"):
+    agent.saveModel()
+        
+        
+
